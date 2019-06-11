d2b15f0b7d8ff12e2b445766a827a7d6a46b18d4
SebastianBukowski <seb-27@o2.pl>
2019-06-11 22:54:13 +0200

    Add of react tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      players: []
+		}
+	}
+	
+	onScoreUpdate = (playerIndex, scoreChange) => {
+		this.setState({
+			players: this.state.players.map((player, index) => {
+				if (index === playerIndex) {
+					return {...player, score: player.score + scoreChange};
+				}
+				return player;
+			})
+		});
+	};
+
+onPlayerAdd = (playerName) => {
+  const newPlayer = {
+    name: playerName,
+    score: 0,
+  }
+		this.setState({
+			players: [...this.state.players, newPlayer]
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<AddPlayer onPlayerAdd={this.onPlayerAdd} />
+				<PlayersList 
+					players={this.state.players} 
+					onScoreUpdate={this.onScoreUpdate}
+					onPlayerRemove={this.onPlayerRemove} 
+				/>
+			</div>
+		);
+	}
 }
+
+
 
 export default App;
